20f0b225ad173d8c070055a30c6da2d39e9840e0
Florencia Do Nascimento <mflorenciadn@gmail.com>
2020-10-23 03:27:57 -0300

    more initial logic

--- assets/gameLogic.js
+++ assets/gameLogic.js
@@ -1,4 +1,9 @@
 const cards = require('../data/cardsArray');
+const player = require('../data/Models/player');
+const room = require('../data/Models/room');
+const round = require('../data/Models/round');
+
+const rooms = [];
 
 function selectCard(color) { 
     let selected = '';
@@ -11,7 +16,6 @@ function selectCard(color) {
         selected = random(cards.blackContent);   
     }
 
-
     return selected;
 }
 
@@ -22,6 +26,56 @@ function random(array){
     return select;
 }
 
+function createRoom(roomId){
+    const myRoom = new room.Room(roomId);
+    rooms.push(myRoom);
+    return myRoom;
+}
 
-let selected = selectCard('white');
-console.log(selected);
+function createPlayer(name, roomId){
+    const myPlayer = new player.Player(name, roomId);
+    const myRoom = rooms.find(room => room.id === roomId);
+    myRoom.players.push(myPlayer);
+    return myPlayer;
+}
+
+
+function createPlay(room) {
+    if(room.player.length() >= MIN_PLAYERS && room.player.length() <= MAX_PLAYERS){
+        if(room.rounds <= MAX_ROUNDS){
+            createRound(room.id);
+            room.rounds++;
+        }
+    } else {
+        //.....
+  }
+}
+
+function createRound(roomId) {
+    const myRound = new round.Round('1234');
+    console.log(myRound);
+    const myRoom = rooms.find(room => room.id === roomId);
+    console.log(myRoom);
+    //myRound.zar = random(myRoom.players);
+    //console.log(zar);
+    //......
+    return myRound;
+}
+
+//let selected = selectCard('white');
+//console.log(selected);
+
+
+let testRoom= createRoom('1234');
+console.log(testRoom);
+let testPlayer = createPlayer('Pepe', '1234');
+console.log(testPlayer);
+console.log(testRoom);
+
+console.log(rooms);
+
+
+
+
+
+
