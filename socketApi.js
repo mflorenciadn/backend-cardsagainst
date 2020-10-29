9782d743bb6b28973dc027dc715b60f4897f97e3
srsenior <rdrg.000@gmail.com>
2020-10-29 17:10:53 -0300

    Chat message

--- socketApi.js
+++ socketApi.js
@@ -12,13 +12,43 @@ const server = http.createServer(app);
 
 const io = socketIo(server);
 
+const Player = require('Player');
+
 
 io.on("connection", (socket) => {
-  console.log("New client connected (back)");
+  //mensaje al que se conecta
+  socket.emit('message', 'Bienvenido a Cards Agains ORT');
+  
+  //mensaje a todos menos al que se conecta
+  // socket.on('connect', () => {
+  //   socket.broadcast.emit('message', 'Un usuario ingreso')
+  // })
+  
+
+  //mensaje a todos en general
+  //io.emit()
+
+  socket.on('chat message', msg => {
+    console.log(msg);
+    io.emit('chat message', msg);
+  })
+
   socket.on("disconnect", () => {
-    console.log("Client disconnected (back)");
+    io.emit('message', 'Un usuario salio de la sala')
   });
 });
+
+// io.on('connection', (socket) => {
+//   socket.join('room 237', () => {
+//     let rooms = Object.keys(socket.rooms);
+//     console.log(rooms); // [ <socket.id>, 'room 237' ]
+//     io.to('room 237').emit('a new user has joined the room'); // broadcast to everyone in the room
+//   });
+// });
+
+
+
+
 
 
   
